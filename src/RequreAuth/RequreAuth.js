a938b3659274e13658cb2c1c428be636ac9d3df8
imran-hossain <csebubt35@gmail.com>
2022-04-17 01:41:16 +0600

    private rote refresh redirect fixa

--- src/RequreAuth/RequreAuth.js
+++ src/RequreAuth/RequreAuth.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Navigate, useLocation } from 'react-router-dom';
+import Loading from '../Components/Loading/Loading';
 import { auth } from '../firebase.init';
 
 const RequreAuth = ({children}) => {
     const [user, loading, error] = useAuthState(auth);
     const location=useLocation()
+    if(loading)
+    {
+        return <Loading></Loading>
+    }
     if (!auth.user) {
         // Redirect them to the /login page, but save the current location they were
         // trying to go to when they were redirected. This allows us to send them
