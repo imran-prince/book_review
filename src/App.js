a4f105848fb2c8e41e191d77ae3dc6bfd9700c1e
imran-hossain <csebubt35@gmail.com>
2022-04-17 00:26:25 +0600

    header add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+ 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './Components/Home/Home';
+import Header from './Components/Header/Header';
+import About from './Components/About/About';
+import Login from './Components/Login/Login';
+import Blog from './Components/Blog/Blog';
+ 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  >
+      <Header></Header>
+     
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/about' element={< About/>}></Route>
+        <Route path='/blog' element={<Blog></Blog>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+      </Routes>
+       
     </div>
   );
 }
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+ 
+import CustomLink from '../CustomLink/CustomLink';
+ 
+
+const Header = () => {
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
+            <Container>
+                <Navbar.Brand  as={CustomLink} to="/">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                     
+                    <Nav className="ms-auto">
+                        <Nav.Link as={CustomLink} to="/about">About</Nav.Link>
+                        <Nav.Link as={CustomLink} to="/blog">Blog</Nav.Link>
+                        <Nav.Link   as={CustomLink} to="/login">
+                            Login
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Header;
